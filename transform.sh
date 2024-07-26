87afaa4187b2fac7e7c1d54a5fa31e8919a5df8c
Styopa Syomin <s.semine@uqconnect.edu.au>
2024-07-26 22:39:00 +1000

    Fixed log

--- transform.sh
+++ transform.sh
@@ -52,8 +52,8 @@ fi
 # Writes required information from input column to .ma file if gene name matches
 log "	Writing data to $gene_dir/$name"
 if [ -f "$snps" ]; then
-	log "	Using $snps tp filter SNPs"
-	awk -v "col1=$snp_id_idx" \
+    log "	Using $snps to filter SNPs"
+    awk -v "col1=$snp_id_idx" \
         -v "col2=$allele_one_idx" \
         -v "col3=$allele_two_idx" \
         -v "col4=$freq_idx" \
@@ -63,9 +63,10 @@ if [ -f "$snps" ]; then
         -v "gidx=$gene_name_idx" \
         -v "cidx=$chr_idx" \
         -v "gene_name=$gene_name" \
-        -v gene_dir="$gene_dir" \
-        -v name="$name" \
-        -v snps="$snps" \
+        -v "gene_dir=$gene_dir" \
+        -v "name=$name" \
+        -v "snps=$snps" \
+        -v "chr_num=$chr_num" \
         'BEGIN {
             while ((getline < snps) > 0) {
                 snp_list[$1]
@@ -76,41 +77,41 @@ if [ -f "$snps" ]; then
                 print $col1, $col2, $col3, $col4, $col5, $col6, $col7 > (gene_dir "/" name)
             }
         }' "$infile" \
-		|| log "transform.sh Error: awk could not write data to $gene_dir/$name" && exit 1 
+        || { log "transform.sh Error: awk could not write data to $gene_dir/$name"; exit 1; }
 else
-	log "	No SNP filter"
-	awk -v "col1=$snp_id_idx" \
-		-v "col2=$allele_one_idx" \
-		-v "col3=$allele_two_idx" \
-		-v "col4=$freq_idx" \
-		-v "col5=$effect_size_idx" \
-		-v "col6=$se_idx" \
-		-v "col7=$p_value_idx" \
-		-v "gidx=$gene_name_idx" \
-		-v "cidx=$chr_idx" \
-		-v "gene_name=$gene_name" \
-		-v gene_dir="$gene_dir" \
-		-v name="$name" \
-		-v name_chr="${gene_name}_chr.txt" \
-		'{
-			if ($gidx == gene_name) {
-				print $col1, $col2, $col3, $col4, $col5, $col6, $col7 > (gene_dir "/" name)
-				print $gene_name, $cidx > (gene_dir "/" name_chr)
-			}
-		}' "$infile" \
-		|| log "transform.sh Error: awk could not write data to $gene_dir/$name" && exit 1
+    log "	No SNP filter"
+    awk -v "col1=$snp_id_idx" \
+        -v "col2=$allele_one_idx" \
+        -v "col3=$allele_two_idx" \
+        -v "col4=$freq_idx" \
+        -v "col5=$effect_size_idx" \
+        -v "col6=$se_idx" \
+        -v "col7=$p_value_idx" \
+        -v "gidx=$gene_name_idx" \
+        -v "cidx=$chr_idx" \
+        -v "gene_name=$gene_name" \
+        -v "gene_dir=$gene_dir" \
+        -v "name=$name" \
+        -v "name_chr=${gene_name}_chr.txt" \
+        '{
+            if ($gidx == gene_name) {
+                print $col1, $col2, $col3, $col4, $col5, $col6, $col7 > (gene_dir "/" name)
+                print gene_name, $cidx > (gene_dir "/" name_chr)
+            }
+        }' "$infile" \
+        || { log "transform.sh Error: awk could not write data to $gene_dir/$name"; exit 1; }
 fi
 
 log "	Data written to $gene_dir/$name"
 
 # Sorts the temporary file by SNP
-sort -k 1 "$gene_dir/$name" \
-	|| log "transform.sh Error: sort could not sort $gene_dir/$name" && exit 1
+sort -k 1 "$gene_dir/$name" -o "$gene_dir/$name" \
+    || { log "transform.sh Error: sort could not sort $gene_dir/$name"; exit 1; }
 
 # Adds SNP count to N column, writes data to a new file
 awk 'NR==FNR {data[$1] = $0; next} $1 in data {print data[$1], $2}' \
-	"$gene_dir/$name" snp_count.txt > "$gene_dir/$name_final" \
-	|| log "transform.sh Error: awk could not write data to $gene_dir/$name_final" && exit 1
+    "$gene_dir/$name" snp_count.txt > "$gene_dir/$name_final" \
+    || { log "transform.sh Error: awk could not write data to $gene_dir/$name_final"; exit 1; }
 
 log "	SNP count added to $gene_dir/$name_final"
 
@@ -119,7 +120,7 @@ rm "$gene_dir/$name"
 
 # Adds column headers to .ma file
 awk -v "cols=$columns" 'BEGIN{print cols}1' "$gene_dir/$name_final" > temp \
-	|| log "transform.sh Error: awk could not add columns to $gene_dir/$name_final" && exit 1
+	|| { log "transform.sh Error: awk could not add columns to $gene_dir/$name_final"; exit 1; }
 	
 mv temp "$gene_dir/$name_final"
 log "	Columns added to $gene_dir/$name_final"
